8e93ab46529cfbf4aae256a251d9a60e09f098f2
sebl311 <sebl01010101@gmail.com>
2024-03-04 15:38:08 +0100

    HelloWorld

--- RabbitMQ/directCom/Send.java
+++ RabbitMQ/directCom/Send.java
@@ -12,10 +12,12 @@ public class Send {
         try (Connection connection = factory.newConnection();
             Channel channel = connection.createChannel()) {
 
+                channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+                String message = "Hello World!";
+                channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+                System.out.println(" [x] Sent '" + message + "'");    
+
         }
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-        String message = "Hello World!";
-        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
-        System.out.println(" [x] Sent '" + message + "'");
+        
     }
 }
