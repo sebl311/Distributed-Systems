6bdeb96a3a3bc3aec86dca68adf338113ee2bcaa
Jad Berjawi <94532128+jadberjawi@users.noreply.github.com>
2024-03-28 17:02:40 +0100

    Update Message.java

--- Overlay/Message.java
+++ Overlay/Message.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Map;
 
 public class Message {
@@ -6,7 +7,7 @@ public class Message {
     private int sourceID;
     private MessageType messageType;
 
-    private enum MessageType {
+    public enum MessageType {
         DATA,
         ROUTING,
         FORWARD
@@ -16,16 +17,22 @@ public class Message {
 
     public Message(String unparsedMessage){
         String[] messageParts = unparsedMessage.split("_");
+
         this.message = messageParts[0].split(":")[1];
+
         this.destinationID = Integer.parseInt(messageParts[1].split(":")[1]);
+
         this.sourceID = Integer.parseInt(messageParts[2].split(":")[1]);
+
         this.messageType = MessageType.valueOf(messageParts[3].split(":")[1]);
-        String[] distanceVectorString = messageParts[4].split(":")[1].split(",");
-        int[] distanceVector = new int[distanceVectorString.length];
-        for(int i = 0; i < distanceVectorString.length; i++){
-            distanceVector[i] = Integer.parseInt(distanceVectorString[i]);
+
+        String distanceVectorString = messageParts[4].split(":")[1];
+        String first = distanceVectorString.substring(1, distanceVectorString.length() - 1);
+        String[] distanceVectorStringElements = first.split(", ");
+        distanceVector = new int[distanceVectorStringElements.length];
+        for(int i = 0; i < distanceVectorStringElements.length; i++){
+            distanceVector[i] = Integer.parseInt(distanceVectorStringElements[i]);
         }
-        this.distanceVector = distanceVector;
     }
     
     public Message(MessageType messageType,String Message, int destinationID, int sourceID, int[] distanceVector){
@@ -48,8 +55,11 @@ public class Message {
     public MessageType getMessageType(){
         return messageType;
     }
+    public int[] getDistanceVector(){
+        return distanceVector;
+    }
     public String toString(){
-        return "Message:" + message + "_Destination:" + destinationID + "_Source:" + sourceID + "_Type:" + messageType + "_DistanceVector:" + distanceVector.toString();
+        return "Message:" + message + "_Destination:" + destinationID + "_Source:" + sourceID + "_Type:" + messageType + "_DistanceVector:" + Arrays.toString(distanceVector);
     }
 
 }
